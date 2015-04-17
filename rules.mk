aosp-mirror/platform_external_openssl

Add support for building crypto for trusty
openssl.config: Sources new openssl.trusty.config

openssl.trusty.config: Trusty specific config variables.

import_openssh.sh:
 * Generate Crypto-trusty-config.mk
 * Generate build-config-trusty.mk
 * New ./Configure target to generate opensslconf-trusty.h

rules.mk: Trusty build system makefile. Uses *-trusty*.mk's generated by
import_openssl.sh.

Change-Id: I4a0c6c61ee2dea45adb4bdfcb36628b9671bbd7f

Conflicts:
	import_openssl.sh

rules.mk
@@ -1,0 +1,30 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+MODULE_USER := true
+
+# get openssl_cflags
+MODULE_SRCDEPS += $(LOCAL_DIR)/build-config-trusty.mk
+include $(LOCAL_DIR)/build-config-trusty.mk
+
+# get target_c_flags, target_c_includes, target_src_files
+MODULE_SRCDEPS += $(LOCAL_DIR)/Crypto-trusty-config.mk
+TARGET_ARCH := $(ARCH)
+include $(LOCAL_DIR)/Crypto-trusty-config.mk
+
+MODULE_SRCS += $(addprefix $(LOCAL_DIR)/,$(common_src_files) $(arm_src_files))
+
+MODULE_CFLAGS += $(target_c_flags)
+MODULE_CFLAGS += -Wno-error=implicit-function-declaration
+
+# Global for other modules which include openssl headers
+GLOBAL_CFLAGS += -DOPENSSL_SYS_TRUSTY
+
+target_c_includes := $(patsubst external/openssl/%,%,$(target_c_includes))
+GLOBAL_INCLUDES += $(addprefix $(LOCAL_DIR)/,$(target_c_includes))
+
+MODULE_DEPS := \
+	lib/openssl-stubs \
+	lib/libc-trusty
+
+include make/module.mk

Whitehawkx/external_openssl

rules.mk
@@ -1,0 +1,30 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+MODULE_USER := true
+
+# get openssl_cflags
+MODULE_SRCDEPS += $(LOCAL_DIR)/build-config-trusty.mk
+include $(LOCAL_DIR)/build-config-trusty.mk
+
+# get target_c_flags, target_c_includes, target_src_files
+MODULE_SRCDEPS += $(LOCAL_DIR)/Crypto-config-trusty.mk
+TARGET_ARCH := $(ARCH)
+include $(LOCAL_DIR)/Crypto-config-trusty.mk
+
+MODULE_SRCS += $(addprefix $(LOCAL_DIR)/,$(LOCAL_SRC_FILES_arm))
+
+MODULE_CFLAGS += $(LOCAL_CFLAGS)
+MODULE_CFLAGS += -Wno-error=implicit-function-declaration
+
+# Global for other modules which include openssl headers
+GLOBAL_CFLAGS += -DOPENSSL_SYS_TRUSTY
+
+LOCAL_C_INCLUDES := $(patsubst external/openssl/%,%,$(LOCAL_C_INCLUDES))
+GLOBAL_INCLUDES += $(addprefix $(LOCAL_DIR)/,$(LOCAL_C_INCLUDES))
+
+MODULE_DEPS := \
+	lib/openssl-stubs \
+	lib/libc-trusty
+
+include make/module.mk